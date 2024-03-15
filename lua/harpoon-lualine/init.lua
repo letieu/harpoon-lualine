159007b4d6caa3cbccc505b1b4ccb9a696dc4707
letieu <letieu8@gmail.com>
2024-03-15 12:16:32 +0700

    feat: add simple status

--- lua/harpoon-lualine/init.lua
+++ lua/harpoon-lualine/init.lua
@@ -4,7 +4,27 @@ local harpoon = utils.lazy_require("harpoon")
 local M = {}
 
 M.status = function()
-  return [[ hehe ]]
+  local char = { "a", "s", "q", "w" }
+
+  local list = harpoon:list()
+  local root_dir = list.config:get_root_dir()
+  local current_file_path = vim.api.nvim_buf_get_name(0)
+
+  local length = list:length()
+
+  local status = ""
+
+  for i = 1, length do
+    local value = list:get(i).value
+    local full_path = root_dir .. "/" .. value
+    if full_path == current_file_path then
+      status = status .. " " .. char[i]:upper()
+    else
+      status = status .. " " .. char[i]
+    end
+  end
+
+  return [[ 󰀱 ]] .. status
 end
 
 return M
