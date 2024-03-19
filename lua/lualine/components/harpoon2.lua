15d2a492498055639889d96993f45ce254e338d1
Taken <taken@mairimashita.org>
2024-03-19 20:39:36 +0100

    Fix for separator

--- lua/lualine/components/harpoon2.lua
+++ lua/lualine/components/harpoon2.lua
@@ -1,27 +1,27 @@
-local lualine_require = require('lualine_require')
-local M = lualine_require.require('lualine.component'):extend()
+local lualine_require = require("lualine_require")
+local M = lualine_require.require("lualine.component"):extend()
 
 local hl = require("harpoon-lualine")
 
 local default_options = {
-  icon = '󰀱 ',
-  indicators = { "1", "2", "3", "4" },
-  active_indicators = { "[1]", "[2]", "[3]", "[4]" },
-  separator = " ",
+    icon = "󰀱 ",
+    indicators = { "1", "2", "3", "4" },
+    active_indicators = { "[1]", "[2]", "[3]", "[4]" },
+    _separator = " ",
 }
 
 function M:init(options)
-  M.super.init(self, options)
-  self.options = vim.tbl_deep_extend('keep', self.options or {}, default_options)
+    M.super.init(self, options)
+    self.options = vim.tbl_deep_extend("keep", self.options or {}, default_options)
 end
 
 function M:update_status()
-  local harpoon_loaded = package.loaded["harpoon"] ~= nil
-  if not harpoon_loaded then
-    return "Harpoon not loaded"
-  end
+    local harpoon_loaded = package.loaded["harpoon"] ~= nil
+    if not harpoon_loaded then
+        return "Harpoon not loaded"
+    end
 
-  return hl.status(self.options)
+    return hl.status(self.options)
 end
 
 return M
