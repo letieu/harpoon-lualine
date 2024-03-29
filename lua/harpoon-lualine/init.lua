23bc1e97a122b8b1f4384287f90754d7aafb41b7
simachri <marvin.christe@posteo.de>
2024-03-29 04:01:06 +0100

    fix: active window not detected for absolute paths outside cwd

--- lua/harpoon-lualine/init.lua
+++ lua/harpoon-lualine/init.lua
@@ -4,17 +4,23 @@ local harpoon = utils.lazy_require "harpoon"
 local M = {}
 
 M.status = function(options)
-    local list = harpoon:list()
-    local root_dir = list.config:get_root_dir()
+    local harpoon_entries = harpoon:list()
+    local root_dir = harpoon_entries.config:get_root_dir()
     local current_file_path = vim.api.nvim_buf_get_name(0)
 
-    local length = math.min(list:length(), #options.indicators)
+    local length = math.min(harpoon_entries:length(), #options.indicators)
 
     local status = {}
 
     for i = 1, length do
-        local value = list:get(i).value
-        local full_path = utils.get_full_path(root_dir, value)
+        local harpoon_path = harpoon_entries:get(i).value
+
+        local full_path = nil
+        if utils.is_relative_path(harpoon_path) then
+            full_path = utils.get_full_path(root_dir, harpoon_path)
+        else
+            full_path = harpoon_path
+        end
 
         if full_path == current_file_path then
             table.insert(status, options.active_indicators[i])
